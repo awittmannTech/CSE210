8af14f0204257bdd44436058b0f1e26aa0393af0
Andrew Wittmann <awittmann.tech@gmail.com>
2026-01-14 19:33:09 -0700

    refactor: implement grade calculation and display in Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        string gradeLetter;
+
+        Console.Write("Enter your grade percentage to get a letter grade: ");
+        string gradeInput = Console.ReadLine();
+
+        int gradePercentage = int.Parse(gradeInput);
+
+        if(gradePercentage >= 90)
+        {
+            gradeLetter = "A";
+        } else if(gradePercentage >= 80)
+        {
+            gradeLetter = "B";
+        } else if(gradePercentage >= 70)
+        {
+            gradeLetter = "C";
+        } else if(gradePercentage >= 60)
+        {
+            gradeLetter = "D";
+        } else if(gradePercentage < 60)
+        {
+            gradeLetter = "F";
+        } else
+        {
+            gradeLetter = "undefined";
+        }
+
+        if (gradeLetter != "F")
+        {
+            if(gradeLetter != "A" && ((gradePercentage % 10) <= 7))
+            {
+                gradeLetter += "+";
+            }else if((gradePercentage % 10) <= 3)
+            {
+                gradeLetter += "-";
+            }
+        }
+
+
+        Console.WriteLine($"Your grade letter is an {gradeLetter}");
     }
 }
