fbf98425accd07d88da40f4d2a0c157d12a20d45
awittmannTech <awittmann.tech@gmail.com>
2026-01-14 20:16:49 -0700

    feat: implement number guessing game in Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response = "yes";
+        int highScore = 9999999;
+
+        while (response == "yes")
+        {
+            string guess = "0";
+            int guessCnt = 0;
+
+            Random randomNumGenerator = new Random();
+            int magicNumber = randomNumGenerator.Next(1, 11);
+
+            while ( int.Parse(guess) != magicNumber )
+            {
+                Console.Write("What is your guess? ");
+                guess = Console.ReadLine();
+
+                if(int.Parse(guess) != magicNumber)
+                {
+                    Console.Write("Nope, guess again... ");
+                }
+
+                guessCnt += 1;
+            }
+
+            Console.WriteLine($"You guess it! It was {magicNumber}!");
+            Console.WriteLine($"It took you {guessCnt} tries.");
+
+            if(guessCnt < highScore)
+            {
+                Console.WriteLine($"You set the new high score!");
+                highScore = guessCnt;
+            } else
+            {
+                Console.WriteLine($"High Score: {highScore}");
+            }
+
+            Console.WriteLine("Respond 'yes' to play again: ");
+            response = Console.ReadLine();
+
+        }
+
+        Console.WriteLine("Goodbye!");
     }
 }
